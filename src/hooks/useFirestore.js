3e31df3b540e430227d754572df6284f56359af3
devhan-hub <hanabduman@gmail.com>
2024-10-12 23:54:50 +0300

    Create custom hook for fetching and listening to Firestore collection data

---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import { firebaseFirestore } from '../firebase/Config';
+import { collection, query, orderBy, onSnapshot } from 'firebase/firestore';
+
+const useFirestore = (collectionName) => {
+  const [docs, setDocs] = useState([]);
+
+  useEffect(() => {
+    const collRef = collection(firebaseFirestore, collectionName);
+    const q = query(collRef, orderBy('createdAt', 'desc')); 
+
+    const unsub = onSnapshot(q, (snap) => {
+      let documents = [];
+      snap.forEach(doc => {
+        documents.push({ ...doc.data(), id: doc.id });
+      });
+      setDocs(documents);
+    });
+
+    // return () => unsub();
+  }, [collectionName]);
+
+  return { docs };
+}
+
+export default useFirestore;
