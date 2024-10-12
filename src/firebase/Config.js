9db4fb5830e0e554fd3d3502ba25190912f0a876
devhan-hub <hanabduman@gmail.com>
2024-10-12 23:52:11 +0300

    Initialize Firebase app with Firestore and Storage configurations

---
+++ src/firebase/Config.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage"; 
+import { getFirestore } from "firebase/firestore";
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBXTg9LiVe7-LVHl9PdL0gcRqoMQlz76GI",
+
+  authDomain: "gallery-updated.firebaseapp.com",
+
+  projectId: "gallery-updated",
+
+  storageBucket: "gallery-updated.appspot.com",
+
+  messagingSenderId: "1096969150047",
+
+  appId: "1:1096969150047:web:114e8c1546ee7bff2ac5ea"
+
+};
+
+
+const app = initializeApp(firebaseConfig);
+const firebaseStorage = getStorage(app)
+const firebaseFirestore = getFirestore(app)
+export {firebaseStorage ,firebaseFirestore}
+
+
