d855433308517d2f956d7cb9eca7ab3339f5bfc6
devhan-hub <hanabduman@gmail.com>
2024-10-15 22:52:44 +0300

    Adding NewAlbum component to add new album

---
+++ src/components/NewAlbum.js
@@ -0,0 +1,36 @@
+import GenericDialog from './GenericDialog';
+import { useState } from 'react';
+import {TextField} from '@mui/material';
+import {  firebaseFirestore } from '../firebase/Config';
+import { setDoc ,doc } from 'firebase/firestore';
+export const NewAlbum = ({ open ,setOpen}) => {
+    const [title, setTitle] = useState('');
+    const handleSubmit = (e) => {
+      if (title) {
+        const fav = doc(firebaseFirestore, 'albums', title); 
+        setDoc(fav, {
+          name:title,
+          files:[]
+        })
+      }
+     
+  setOpen(false)  };
+  
+    return (
+      <GenericDialog
+        open={open}
+        handleClose={()=> setOpen(false)}
+        title="Add New Album"
+        onSubmit={handleSubmit}
+      >
+        <TextField
+          label="Title"
+          name="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          fullWidth
+          required
+        />
+      </GenericDialog>
+    );
+  };
