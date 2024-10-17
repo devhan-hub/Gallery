db267392a538cb2046ea7912ce01d318631fc1d9
devhan-hub <hanabduman@gmail.com>
2024-10-18 00:20:26 +0300

    feat: conditionally render login form or main content based on user authentication in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,17 @@
 import React, { useEffect, useReducer, useState } from 'react';
-import Navbar from './Navbar';
-import AlbumView from './AlbumView';
-import Video from './Video';
-import { Route, Routes , useLocation} from 'react-router-dom';
-import {AnimatePresence} from 'framer-motion'
-import PictureDisplay from './PictureDisplay';
-import Notfound from './Notfound';
-import UploadForm from './UploadForm';
+import Main from './Main';
+import LoginForm from './LoginForm'
+import useAuth from '../hooks/useAuth'
 
 const App = () => {
-
-  const location = useLocation();
-  useEffect(()=> {
-
-  })
+const [user] = useAuth();
   return (  
-      <div className="min-h-screen bg-gray-100 space-y-0">
-        <Navbar />
-        <UploadForm/>
-        <AnimatePresence>
-        <Routes location={location} key={location.key}>
-          <Route path='/' element={<PictureDisplay/>} />
-
-          <Route path='/albums' element={<AlbumView />} />
-           <Route path='/videoss' element={<Video />} />
-          {/* // <Route path='*' element={<Notfound  />} /> */ }
-        </Routes>
-        </AnimatePresence>
-      </div> 
+     
+     <div>
+      {
+        user?<Main/>:<LoginForm/>
+      }
+     </div>
   );
 };
 
