54989b96f06d49fa9b24a74524665dbdbcb320eb
devhan-hub <hanabduman@gmail.com>
2024-10-18 00:24:42 +0300

    adding useAuth.js to listen auth state change

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react'
+import {firebaseAuth}  from '../firebase/Config'
+import { onAuthStateChanged } from 'firebase/auth';
+const useAuth = () => {
+ const [user , setUser]= useState(null)
+    useEffect(()=> {
+        const unsubscribe = onAuthStateChanged((firebaseAuth) , (user)=>{
+           if(user){
+             setUser(user)
+     }
+           else{
+             setUser(null)
+           }
+          
+        })
+        return () => unsubscribe();
+       }, []);
+         return ( [user])
+}
+
+export default useAuth
