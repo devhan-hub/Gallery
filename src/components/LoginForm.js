419bda495e5158176b34d5db1e606a8abf4cec40
devhan-hub <hanabduman@gmail.com>
2024-10-25 10:33:34 +0300

    update loginPage UI

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,27 +1,31 @@
 import { useState } from 'react';
-import { Button, TextField, Box, Container, Typography, Link, Snackbar } from '@mui/material';
+import { Button, TextField, Box, Typography, Link } from '@mui/material';
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { firebaseAuth, firebaseFirestore } from '../firebase/Config';
-import { motion } from 'framer-motion';
 import { styled } from '@mui/material/styles';
-import { addDoc, doc, setDoc } from 'firebase/firestore';
+import {  doc, setDoc } from 'firebase/firestore';
 import { SignInValidate, SignUpValidate } from './Validate';
 
-const TextFieldStyled = styled(TextField)( {
-  '& .MuiOutlinedInput-root': {
-    '& fieldset': {
-      borderColor: '#ff6f61',
+const TextFieldStyled = styled(TextField)({
+  '& .MuiFilledInput-root': {
+    backgroundColor: 'white',
+    borderRadius: 4, 
+    '&:hover': {
+      backgroundColor: '#f9f9f9',
     },
-    '&:hover fieldset': {
-      borderColor: '#ff6f61',
+    '&.Mui-focused': {
+      backgroundColor: 'white',
     },
-    '&.Mui-focused fieldset': {
-      borderColor: '#ff6f61',
+    '&:before': {
+      borderBottom: '1px solid #ff6f61', 
+    },
+    '&:after': {
+      borderBottom: '2px solid #ff6f61',
     },
   },
   '& .MuiInputLabel-root': {
-    color: '#ff6f61',
+    color: 'black',
   },
   '& .MuiInputLabel-root.Mui-focused': {
     color: '#ff6f61',
@@ -91,51 +95,38 @@ const LoginForm = () => {
   };
 
   return (
-    <div>
-      <div className='flex justify-between items-center px-4 mt-2'>
-        <a href="" className='ml-2 inline-block'>
-          <Link path=''><img src='Images/logo6.png' className='-m-16 -ml-14'></img></Link>
-        </a>
-        <a href="#sign" className='md:hidden text-xl text-[#ff6f61] font-extrabold'>Sign In</a>
-      </div>
-      <div className='flex flex-col md:flex-row items-center px-10 md:px-6 lg:px-10'>
-        <motion.div className='relative'
-          initial={{ left: '-1000px' }}
-          animate={{ left: '0' }}
-          transition={{ duration: 0.5 }}>
-          <p className='text-3xl mx-auto italic text-[#ff6f61] p-2 text-center md:mt-0 mt-8'>Turn Moments into Memories. Sign Up to Start.</p>
-          <img src="Images/gallery2.jpg" alt="" />
-        </motion.div>
-        <Container maxWidth='sm' className='mt-16 shadow-2xl border-2 border-[#f59c94]' id='sign'>
+    <div className="h-screen bg-cover bg-opacity-50 relative" style={{ backgroundImage: "url('/Images/galery3.png')" }}>
+    {/* Your content here */}
+<div className='flex flex-col  items-center gap-0  w-full fixed bg-black bg-opacity-65 h-screen p-4'>
+        <img src="Images/logow.png" alt=""  className=' max-w-80 '/>
           <Box
+          id='sign'
             sx={{
               display: 'flex',
               flexDirection: 'column',
               alignItems: 'center',
               justifyContent: 'center',
-              marginTop: 8,
               padding: 2,
               borderRadius: '8px',
-              backgroundColor: 'white'
+              marginTop:'-19px'
             }}
           >
-            <Typography variant="h5" gutterBottom sx={{ color: '#ff6f61' }}>
-              {isLogin ? <span>Sign In</span> : <span>Create new account</span>}
+            <Typography variant="h5" gutterBottom sx={{ color: 'white' }}>
+              {isLogin ? <span>Welcome back : ) </span> : <span className=''>Register</span>}
             </Typography>
-            <form className='w-full space-y-8 p-2 '>
+            <form className='w-full flex flex-col gap-0  space-y-4 p-2 '>
               {!isLogin && (
                 <div className='flex items-center gap-2'>
                   <TextFieldStyled
-                    required
-                    id="outlined-required"
+                    
+                 variant='filled'
                     label="Firstname"
                     margin='normal'
                     value={firstName}
                     onChange={(e) => setFirstName(e.target.value)}
                   />
                   <TextFieldStyled
-                    required
-                    id="outlined-required"
+                 variant='filled'
                     label="LastName"
                     margin='normal'
                     value={lastName}
@@ -144,28 +135,24 @@ const LoginForm = () => {
                 </div>
               )}
               <TextFieldStyled
-                required
-                id="outlined-required"
+             variant='filled'
                 label="Email"
                 margin='normal'
-                fullWidth
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
               <TextFieldStyled
-                required
-                variant='outlined'
+                
+              variant='filled'
                 label='Password'
                 type='password'
                 margin='normal'
-                fullWidth
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
               {!isLogin && (
                 <TextFieldStyled
-                  required
-                  variant='outlined'
+                variant='filled'
                   label='Confirm Password'
                   type='password'
                   margin='normal'
@@ -193,7 +180,7 @@ const LoginForm = () => {
                   Create account
                 </Link>
               ) : (
-                <div className='flex items-center gap-2'>
+                <div className='flex items-center gap-2 text-white'>
                   <span>Already have an account? </span>
                   <Link
                     component="button"
@@ -228,9 +215,8 @@ const LoginForm = () => {
               </Button>
             </form>
           </Box>
-        </Container>
       </div>
-    </div>
+</div>
   );
 };
 
